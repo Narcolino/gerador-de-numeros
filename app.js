f5523020eb76b9bca0768cbe7ebcc451bc88f5ad
Marco Antonio <marcosanto5550123@outlook.com>
2024-12-16 16:38:31 -0400

    adicionando a funcionabilidades

--- app.js
+++ app.js
@@ -6,15 +6,26 @@ function sortear() {
     let sorteados = [];
     let numero;
 
+    let resultado = document.getElementById('resultado');
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio (de,ate);
+        
+        while (sorteados.includes(numero)){
+            numero = obterNumeroAleatorio(de,ate);
+        }
+
         sorteados.push(numero );
     }
 
-    alert(sorteados);
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    
 }
 
 function obterNumeroAleatorio(min,max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function ba() {
+    
+}
